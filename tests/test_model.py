6770cf39dcdfb11f4a6f5c6753ad5e1adffbacce
pmarise <pavan.marisetti@gmail.com>
2025-01-20 22:24:33 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,13 @@
 # tests/test_model.py
+import os  
+import sys 
+current_directory = os.getcwd()  
+print("Current Directory:", current_directory)  
+# current_directory = current_directory+'\mlops-pipeline'
+print("Current Directory:",current_directory) 
+# Get the current working directory  
+sys.path.append(current_directory)
+
 from src.model import predict
 
 
