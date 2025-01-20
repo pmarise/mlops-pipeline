5d0cdaef78b25c0eeafd71971548f67b1f9e7aa9
pmarise <pavan.marisetti@gmail.com>
2025-01-20 22:48:07 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,7 @@
 # tests/test_model.py
 import os
 import sys
-current_directory = os.getcwd()
-sys.path.append(current_directory)
-from src.model import predict
+from /home/runner/work/mlops-pipeline/mlops-pipeline/src.model import predict
 
 def test_predict():
     features = [1, 2, 3]
