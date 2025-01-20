4d1ced37fd8a16ce7a95b034d5bf7b4cb85d67dc
pmarise <pavan.marisetti@gmail.com>
2025-01-20 23:00:14 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,7 @@
 # tests/test_model.py
 import os
 import sys
-from /home/runner/work/mlops-pipeline/mlops-pipeline/src.model import predict
+from src.model import predict
 
 def test_predict():
     features = [1, 2, 3]
